ac3dc18b7053308413f07de3226c224a045f37d2
wawan sismadi <wawan@sismadi.com>
2024-10-15 21:10:58 +0700

    Update gowok.js

--- gowok.js
+++ gowok.js
@@ -1,11 +1,36 @@
+<html>
+<head><title>Halo Gowok</title></head>
+<body><div id="content">Halo Gowok</div>
+<script>
+gebi=function(str){return document.getElementById(str);};
 g={
   model:{
-    login:[{nama:'satu',pin:'123'}]
+    login:[{nama:'satu',pin:'123'}],
   },
+
   service:{
-    getlogin:function(){},
-    setlogin:function(){},
+    setlogin:function(nama){window.localStorage.getItem(nama);},
+    setlogin:function(nama,arr){window.localStorage.setItem(nama, JSON.stringify(arr));},
+    remlogin:function(nama){window.localStorage.removeItem(nama);},
   },
-  view:{},
-  controller:{},
-}
+
+  view:{
+    signform:function(arr){ return `
+      <label>Nama</label><input id:'nama' ><br/>
+      <label>PIN</label><input><br/>
+      <button id='btnSignin'>Signin</button>`;
+    }
+  },
+
+  controller:{
+    signform:function(){
+      gebi('content').innerHTML=g.view.signform();
+      gebi('btnSignin').onclick=function(){alert('Login')}
+    },
+
+  },
+};
+g.controller.signform();
+</script>
+</body>
+</html>
